898229304125382abc26e33fdd09ec5286e10bc0
liber09 <linda.bergsangel@hotmail.com>
2023-10-05 05:24:20 +0200

    export complete wordlist

---
+++ backend/src/words/words.ts
@@ -0,0 +1,9 @@
+import five_letters from "./5_letters";
+import six_letters from "./6_letters";
+import seven_letters from "./7_letters";
+
+const words5Letters = five_letters.words.join(" ");
+const words6Letters = six_letters.words.join(" ");
+const words7Letters = seven_letters.words.join(" ");
+
+export const wordList = words5Letters + words6Letters + words7Letters;
