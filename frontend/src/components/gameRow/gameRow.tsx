43d16e2231a8007cd967dd87af791f28b9227b92
liber09 <linda.bergsangel@hotmail.com>
2023-10-29 21:11:13 +0100

    begin to handle input

--- frontend/src/components/gameRow/gameRow.tsx
+++ frontend/src/components/gameRow/gameRow.tsx
@@ -2,12 +2,25 @@ import React, {useState} from "react";
 import Tile from "../tile/tile";
 
 interface Props {
+    correctWord: string;
     isCorrect: boolean;
     exists: boolean;
     letterArray: string[];
     rowIndex: number;
+    setLetterArray: (letters: string[]) => void;
 }
 
+const [input, setInput] = React.useState("");
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    
+    //https://stackoverflow.com/questions/66663831/key-press-event-in-react-with-typescript
+    //https://www.w3schools.com/jsref/event_key_key.asp
+}
 
 export default function GameRow({isCorrect, exists, letterArray, rowIndex}: Props){
 
